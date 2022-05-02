8016e4140acfcd3203f1c529efda8350f807952c
Arthur Dionízio <arthur.dionizio@redfox.tech>
2022-05-02 18:13:03 -0300

    trying to create a general text with specific optional styles

---
+++ src/components/BottomCard/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { BottomCardContainer } from './styles';
+
+interface BottomCardProps{
+  header: any,
+  content: any,
+  footer: any
+}
+
+export const BottomCard: React.FC<BottomCardProps> = ({
+  header
+}) => {
+  return (
+    <BottomCardContainer>
+      {header}
+    </BottomCardContainer>
+  );
+};
+
+export default BottomCard;
+
